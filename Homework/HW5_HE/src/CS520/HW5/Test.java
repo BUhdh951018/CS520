743fa2aa52c14af7cf0cc4c74006af963e9d1a79
DONGHANG HE <donghanghe@DONGHANGs-MacBook-Pro.local>
2019-11-20 15:42:38 -0500

    hw5

--- Homework/HW5_HE/src/CS520/HW5/Test.java
+++ Homework/HW5_HE/src/CS520/HW5/Test.java
@@ -30,22 +30,32 @@ public class Test {
 			e.printStackTrace();
 		}
 		
+		// use BufferedReader to read the file
 		BufferedReader reader = new BufferedReader(fileReader);
 		
 		try {
+			
+			// read the contents one line at a time
 			String content = reader.readLine().trim();
 			
 			System.out.println("Input file processing...");
 			
+			// the loop
 			while (content != null) {
+				// invoke the method 
 				Student student = processInputData(content);
+				// add student to studentQueue
 				studentQueue.add(student);
+				// insert student into studentMap
 				studentMap.put(student.getName(), student);
+				// read the next line
 				content = reader.readLine();
 			}
 			
+			// iterate the studentQueue 
 			Iterator<Student> queueIterator = studentQueue.iterator();
 			
+			// display each element
 			System.out.println("\nInterating over student list...");
 			
 			while (queueIterator.hasNext()) {
@@ -54,8 +64,10 @@ public class Test {
 			
 			System.out.println("\nInteraing over student map...");
 			
+			// iterate over the keys
 			Iterator<String> keyIterator = studentMap.keySet().iterator();
 			
+			// display the object
 			while (keyIterator.hasNext()) {
 				String nextKeyString = keyIterator.next();
 				System.out.println(studentMap.get(nextKeyString).toString());
@@ -65,15 +77,23 @@ public class Test {
 		}
 	}
 	
+	// method Student(return corresponding Student object)
 	private static Student processInputData(String line) {
+		
+		// tokenize the string use comma delimiter
 		StringTokenizer tokenizer = new StringTokenizer(line, ",");
 		
+		// create student object
 		Student currentStudent = new Student(tokenizer.nextToken());
 		
+		// read the homework grade 
 		while (tokenizer.hasMoreTokens()) {
+			
+			// use the addHomeworkGrade method to add the grade to Student object
 			currentStudent.addHomeworkGrade(Integer.parseInt(tokenizer.nextToken().trim()));
 		}
 		
+		// display the string representation 
 		System.out.println(currentStudent.toString());
 		
 		return currentStudent;
